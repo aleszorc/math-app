5d3e8ff5804656f12afae4a30f684f8b8991587b
Lex-124 <ales.zorc1@gmail.com>
2021-09-19 18:57:42 +0200

    basic setup

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,27 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'math-app';
+  constructor() {}
+
+  first_num: number = this.getRandomNum(50);
+  second_num: number = this.getRandomNum(50);
+
+  result: any;
+
+  sayWorld() {
+    alert('World!');
+  }
+
+  getRandomNum(max: number) {
+    return Math.floor(Math.random() * max);
+  }
+
+  checkResult() {
+    if (parseInt(this.result) === this.first_num + this.second_num) {
+      alert('Pravilno!');
+    }
+  }
 }
